6d0cd93bb372c98fee4c79ff51794aae65827fd8
Evgen <zhenyalezhenin@gmail.com>
2025-04-01 11:44:13 +0300

    begin

---
+++ boilersVite/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Page from "./components/Templates/Page.tsx";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+      <div className="main_wrap">
+        <Page/>
+      </div>
+  )
+}
+
+export default App
